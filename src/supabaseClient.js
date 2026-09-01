4bcf3d91e26ea3b9c609f064f91e2ebd65bc5fac
Tu Nombre <you@example.com>
2026-09-01 12:13:03 -0300

    Corregir API

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://zorxsnqmkprdtkkbjjrh.supabase.co'
-const supabaseAnonKey = 'sb_publishable_-lToeoaix_FlO7SjRxmKKw_kPGbClFd'
+const supabaseAnonKey = 'sb_publishable_7mCG5VZsSQ3OklZ6nwNVhw_bfANPvVJ'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
